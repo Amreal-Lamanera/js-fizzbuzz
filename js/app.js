10d10cd00864ce02683ed3551b99eaef7dfa44b4
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-22 14:27:42 +0200

    add script

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
-alert("js ok");
+// alert("js ok");
 
 // - PER OGNI i che va da 0 a 100
-// - SE i % 3 AND i % 5 === 0
-// - allora stampa FizzBuzz
-// - ALTRIMENTI SE i % 3 === 0
+for (let i = 0; i < 100; i++) {
+    // - SE (i + 1) % 3 AND (i + 1) % 5 === 0 => (i + 1) % 15 === 0
+    if (((i + 1) % 15) === 0) {
+        // - allora stampa FizzBuzz
+        console.log("FizzBuzz")
+    } else {
+        if (((i + 1) % 3) === 0) {
+            console.log("Fizz")
+        } else {
+            if (((i + 1) % 5) === 0) {
+                console.log("Buzz");
+            } else {
+                console.log(i + 1);
+            }
+        }
+    }
+}
+// - ALTRIMENTI SE (i + 1) % 3 === 0
 // - allora stampa Fizz
-// - ALTRIMENTI SE i % 5 === 0
+// - ALTRIMENTI SE (i + 1) % 5 === 0
 // -allora stampa Buzz
 // -ALTRIMENTI stampa i + 1
