e4fd7f9d0c3be0a6c11d3d27c3e470acf26f6785
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-07-25 10:00:51 +0200

    refactoring

--- js/app.js
+++ js/app.js
@@ -41,33 +41,25 @@ const myGrid = document.querySelector('div[class = "my_grid"]');
 for (let i = 0; i < 100; i++) {
     const myCell = document.createElement('div');
     myCell.classList.add('cell');
-
+    let print = i + 1;
     // - SE (i + 1) % 3 AND (i + 1) % 5 === 0 => (i + 1) % 15 === 0
-    if (((i + 1) % 15) === 0) {
+    if ((print % 15) === 0) {
         // - allora stampa FizzBuzz
-        console.log("FizzBuzz");
-        myCell.append('FizzBuzz');
+        print = 'FizzBuzz'
         myCell.classList.add('fizzbuzz');
-    } else {
-        // - ALTRIMENTI SE (i + 1) % 3 === 0
-        if (((i + 1) % 3) === 0) {
-            // - allora stampa Fizz
-            console.log("Fizz");
-            myCell.append('Fizz');
-            myCell.classList.add('fizz');
-        } else {
-            // - ALTRIMENTI SE (i + 1) % 5 === 0
-            if (((i + 1) % 5) === 0) {
-                // -allora stampa Buzz
-                console.log("Buzz");
-                myCell.append('Buzz');
-                myCell.classList.add('buzz');
-            } else {
-                // -ALTRIMENTI stampa i + 1
-                console.log(i + 1);
-                myCell.append(i + 1);
-            }
-        }
+    } // - ALTRIMENTI SE (i + 1) % 3 === 0
+    else if ((print % 3) === 0) {
+        // - allora stampa Fizz
+        print = "Fizz";
+        myCell.classList.add('fizz');
+    } // - ALTRIMENTI SE (i + 1) % 5 === 0
+    else if ((print % 5) === 0) {
+        // -allora stampa Buzz
+        print = "Buzz";
+        myCell.classList.add('buzz');
     }
+    // -ALTRIMENTI stampa i + 1
+    console.log(print);
+    myCell.append(print);
     myGrid.append(myCell);
 }
